cbc7854e29c3ce0fc21b364455481dead85e4710
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2020-01-18 16:42:15 -0800

    Updates

---
+++ precache-manifest.1363f2d6451490a84bd9f0cc2f8161a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47506a4a1b5d75b336f4fb9944693593",
+    "url": "/jchain/index.html"
+  },
+  {
+    "revision": "c555bf68a1e5c9e47aaf",
+    "url": "/jchain/static/css/main.1213308c.chunk.css"
+  },
+  {
+    "revision": "3f28fe582c94ef5e5f65",
+    "url": "/jchain/static/js/2.4373ea4c.chunk.js"
+  },
+  {
+    "revision": "c555bf68a1e5c9e47aaf",
+    "url": "/jchain/static/js/main.4e92e95b.chunk.js"
+  },
+  {
+    "revision": "c91ac73c1e8987f32b96",
+    "url": "/jchain/static/js/runtime~main.72ba0c62.js"
+  },
+  {
+    "revision": "6f6c477614643076e981f02c2930f4d4",
+    "url": "/jchain/static/media/banner-bg.6f6c4776.png"
+  },
+  {
+    "revision": "3c45340da6dcd8eb91aec4f0146581ef",
+    "url": "/jchain/static/media/barrier.3c45340d.svg"
+  },
+  {
+    "revision": "fe5cad5b12c83b8e99810b0e23d829dc",
+    "url": "/jchain/static/media/dev-team.fe5cad5b.jpg"
+  },
+  {
+    "revision": "4b73f89a2da134ec4a82ba8c5f16d688",
+    "url": "/jchain/static/media/handshake.4b73f89a.svg"
+  },
+  {
+    "revision": "9a0ba9109a5f93e957ffae4e43332c55",
+    "url": "/jchain/static/media/laptop.9a0ba910.png"
+  },
+  {
+    "revision": "067ce8cf58da6938ec5c08923f55b0a6",
+    "url": "/jchain/static/media/partnerships.067ce8cf.jpg"
+  },
+  {
+    "revision": "a14f4c4c9028b7f91391c5457a2eb21b",
+    "url": "/jchain/static/media/sdk.a14f4c4c.jpg"
+  },
+  {
+    "revision": "114ab38186a804ad4be93aea9a613be9",
+    "url": "/jchain/static/media/timer.114ab381.svg"
+  }
+]);
