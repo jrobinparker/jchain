689f3dce8d94a182e3ee9aa868936e963faff81c
Jeremy Parker <jrobinparker@gmail.com>
2020-11-10 07:29:18 -0800

    Refactored SocialIcon component

--- src/components/footer/SocialIcon.js
+++ src/components/footer/SocialIcon.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-const SocialIcon = props => {
-  const { icon } = props
-  return (
-    <i className={`fab ${icon}`} />
-  )
+const SocialIcon = ({ icon }) => {
+  return <i className={`fab ${icon}`} />
 }
 
 export default SocialIcon
