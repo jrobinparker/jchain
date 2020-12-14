fd943665d8e1959e327dedda2be172ca157fbe13
Jeremy Parker <jrobinparker@gmail.com>
2020-12-14 05:47:20 -0800

    Style updates to DashboardPage component

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -16,11 +16,18 @@ export default function DashboardPage() {
               <Card.Item>12/14/2020: ITEM PURCHASED</Card.Item>
               <Card.Item>12/14/2020: ITEM PURCHASED</Card.Item>
               <Card.Item>12/14/2020: REFUND TO CUSTOMER</Card.Item>
-              <Card.Item>12/13/2020: ITEM PURCHASED</Card.Item>
+              <Card.Item>12/13/2020: JCHN DEPOSIT</Card.Item>
             </Card.List>
           </Card>
           <Card>
             <Card.Title>Current holdings</Card.Title>
+            <Card.Title>200,000 JCHN</Card.Title>
+            <Card.List>
+              <Card.Item>12/14/2020: 1000 JCHN DEPOSIT</Card.Item>
+              <Card.Item>12/14/2020: 1000 JCHN DEPOSIT</Card.Item>
+              <Card.Item>12/14/2020: 2000 JCHN WITHDRAWAL</Card.Item>
+              <Card.Item>12/13/2020: 30,000 JCHN DEPOSIT</Card.Item>
+            </Card.List>
           </Card>
           <Card>
             <Card.Title>Graph</Card.Title>
